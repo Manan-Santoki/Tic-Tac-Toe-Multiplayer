9fa14df54632a05fb8b0fa2017548440b4170e76
Manan Santoki <manansantoki1702@gmail.com>
2023-09-06 15:25:45 +0530

    Don't run on openshift server

--- server.js
+++ server.js
@@ -304,14 +304,14 @@ io.on('connection', function(socket){
 //Server configuration stuff
 
 //Tries to find variable for openshift ip, if nothing then loads to localhost
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var ipaddress =  "127.0.0.1";
 
 //Same as above, but with port
 //If you want to load on localhost onto a different port, change 4000 to whatever port you please
-var port = process.env.OPENSHIFT_NODEJS_PORT || 4000;
+var port =  4000;
 
 http.listen(port, ipaddress, function(){
-	console.log('Running on Openshift Server')
+	console.log('Running on Local host')
 })
 
 
